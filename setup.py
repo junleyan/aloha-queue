7215408219dbfe763a13ce24ab23bcfcdb756f1e
Junle Yan <yanjunlereal@gmail.com>
2023-09-18 20:56:17 -1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aloha-queue",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Junle Yan",
+    author_email="junleyan.hi@gmail.com",
+    license="MIT"
+)
